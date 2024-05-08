5cbac90a78211bf6cb031824dab6ec491c0d3990
Luca Pascali <l.pascali@4sigma.it>
2024-05-08 12:47:27 +0200

    arrangement

--- cyclock.go
+++ cyclock.go
@@ -19,86 +19,6 @@ const (
 //go:embed assets/*.png assets/*.ttf
 var assets embed.FS
 
-var CircleSurface *sdl.Surface
-
-func drawDot(surface *sdl.Surface, x, y int, radius int) {
-	CircleSurface.Blit(nil, surface, &sdl.Rect{
-		X: int32(x - radius),
-		Y: int32(y - radius),
-		W: 0, H: 0})
-}
-
-func positionAtAngle(cx, cy, a, r0, aq, r1 int) (int, int) {
-	rad := float64(a) / 180.0 * math.Pi
-	rad_aq := float64(aq) / 180.0 * math.Pi
-
-	x := cx + int(float64(r0)*math.Sin(rad)+float64(r1)*math.Sin(rad_aq))
-	y := cy - int(float64(r0)*math.Cos(rad)+float64(r1)*math.Cos(rad_aq))
-
-	return x, y
-}
-
-func drawDial(surface *sdl.Surface, minutes int, paddle *sdl.Surface, dial int) {
-
-	hour := dial % 12
-	minutes += 60 * hour
-
-	a0 := (minutes * 120 / 60) % 360
-	h := minutes / 60
-
-	q := ((h / 3) * 90) % 360
-	aq := q
-
-	rt0 := 120
-	rt1 := 360
-	if a0 >= rt0 {
-		aq += (a0 - rt0) * (90 - 360*2) / (rt1 - rt0)
-	}
-
-	r0 := 190
-	r1 := 260 - r0
-	rd := 20
-
-	a0 -= 60
-	a1 := a0 + aq
-
-	x, y := positionAtAngle(300, 300, a0, r0, a1, r1)
-	drawDot(surface, x, y, rd)
-
-	paddle.Blit(nil, surface, &sdl.Rect{
-		X: int32(x) - (paddle.W / 2),
-		Y: int32(y) - (paddle.H / 2),
-		W: 0, H: 0})
-
-}
-
-var bigT int = 0
-
-const debug bool = false
-
-func getTime() int {
-
-	if debug {
-		bigT += 1
-		bigT %= 720
-		return bigT
-
-	}
-	t := time.Now()
-
-	h := t.Hour() //% 12
-	m := t.Minute()
-
-	return m + h*60
-}
-
-func formatTime(tim int) string {
-	m := tim % 60
-	h := tim / 60
-
-	return fmt.Sprintf("time: %2d:%02d", h, m)
-}
-
 func assetsFont(path string, size int) *ttf.Font {
 	raw, err := assets.ReadFile(path)
 	if err != nil {
@@ -132,6 +52,83 @@ func assetsImage(path string) *sdl.Surface {
 	return surf
 }
 
+func drawDial(surface *sdl.Surface, dot *sdl.Surface, minutes int, paddle *sdl.Surface, dial int) {
+
+	drawDot := func(surface *sdl.Surface, x, y int, radius int) {
+		dot.Blit(nil, surface, &sdl.Rect{
+			X: int32(x - radius),
+			Y: int32(y - radius),
+			W: 0, H: 0})
+	}
+
+	positionAtAngle := func(cx, cy, a, r0, aq, r1 int) (int, int) {
+		rad := float64(a) / 180.0 * math.Pi
+		rad_aq := float64(aq) / 180.0 * math.Pi
+
+		x := cx + int(float64(r0)*math.Sin(rad)+float64(r1)*math.Sin(rad_aq))
+		y := cy - int(float64(r0)*math.Cos(rad)+float64(r1)*math.Cos(rad_aq))
+
+		return x, y
+	}
+
+	hour := dial % 12
+	minutes += 60 * hour
+
+	a0 := (minutes * 120 / 60) % 360
+	h := minutes / 60
+
+	q := ((h / 3) * 90) % 360
+	aq := q
+
+	rt0 := 120
+	rt1 := 360
+	if a0 >= rt0 {
+		aq += (a0 - rt0) * (90 - 360*2) / (rt1 - rt0)
+	}
+
+	r0 := 190
+	r1 := 260 - r0
+	rd := 20
+
+	a0 -= 60
+	a1 := a0 + aq
+
+	x, y := positionAtAngle(300, 300, a0, r0, a1, r1)
+	drawDot(surface, x, y, rd)
+
+	paddle.Blit(nil, surface, &sdl.Rect{
+		X: int32(x) - (paddle.W / 2),
+		Y: int32(y) - (paddle.H / 2),
+		W: 0, H: 0})
+}
+
+var bigT int = 0
+
+const debug bool = false
+
+func getTime() int {
+
+	if debug {
+		bigT += 1
+		bigT %= 720
+		return bigT
+
+	}
+	t := time.Now()
+
+	h := t.Hour()
+	m := t.Minute()
+
+	return m + h*60
+}
+
+func formatTime(tim int) string {
+	m := tim % 60
+	h := tim / 60
+
+	return fmt.Sprintf("time: %2d:%02d", h, m)
+}
+
 func main() {
 	if err := ttf.Init(); err != nil {
 		panic(err)
@@ -143,8 +140,7 @@ func main() {
 	}
 	defer sdl.Quit()
 
-	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-		600, 600, sdl.WINDOW_SHOWN)
+	window, err := sdl.CreateWindow("test", sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED, 600, 600, sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
 	}
@@ -152,7 +148,7 @@ func main() {
 
 	font := assetsFont(fontPath, fontSize)
 	quadrant := assetsImage("assets/quadrant.png")
-	CircleSurface = assetsImage("assets/dot.png")
+	dot := assetsImage("assets/dot.png")
 
 	surface, err := window.GetSurface()
 	if err != nil {
@@ -179,7 +175,6 @@ func main() {
 	}()
 
 	go func() {
-
 		for {
 			if !running {
 				return
@@ -190,7 +185,7 @@ func main() {
 			window.SetTitle(formatTime(minutes))
 
 			for j := range 12 {
-				drawDial(surface, minutes, paddle[j], j)
+				drawDial(surface, dot, minutes, paddle[j], j)
 			}
 
 			quadrant.Blit(nil, surface, &sdl.Rect{
